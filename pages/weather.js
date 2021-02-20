e642005ad9f798777e37aadc4c2cfba7a8d842d6
M. Khairul Makirin <khairul_makirin@yahoo.com>
2021-02-20 21:24:59 +0700

    added text field to weather

--- pages/weather.js
+++ pages/weather.js
@@ -1,5 +1,23 @@
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+
+const CityInput = withStyles({
+  root: {
+    minWidth: "100%"
+  }
+})(TextField);
+
 export default function Weather() {
+  const [city, setCity] = useState("jakarta"); // default city is Jakarta
+
   return (
-    <div className="page"></div>
+    <div className="page">
+      <CityInput
+        id="city-input"
+        label="enter a city's name"
+        variant="filled"
+      />
+    </div>
   )
 }
